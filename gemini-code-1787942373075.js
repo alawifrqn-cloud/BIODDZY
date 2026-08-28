2130a685c766cc89eb1ff649e5d3dfa2a9236cfc
alawifrqn-cloud <alawifrqn@gmail.com>
2026-08-29 01:40:25 +0700

    Add files via upload

---
+++ gemini-code-1787942373075.js
@@ -0,0 +1,4 @@
+// ============================================================
+// 🔑 BARIS 168: MASUKKAN KODE API KEY KAMU DI BAWAH INI
+// ============================================================
+const MY_API_KEY = "PASTE_API_KEY_KAMU_DISINI";
